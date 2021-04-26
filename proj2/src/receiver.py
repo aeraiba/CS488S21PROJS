b781b69a0d986026468b8db38b8efd2985e24636
aeraiba <amireraiba@yahoo.com>
2021-04-26 11:34:48 -0400

    Add files via upload

--- proj2/src/receiver.py
+++ proj2/src/receiver.py
@@ -5,25 +5,65 @@
 from socket import *
 import sys
 import select
-import json 
+import time
+import json
+import base64
+import struct
 
-host="0.0.0.0"
+  
+  
+  
+
+#------------------------------------running main program------------------------------
+#sys.argv for testing
+#host = sys.argv[1]
+host = "127.0.0.1"
+#port = int(sys.argv[2])
 port = int(sys.argv[1])
+
+#start socket and bind to sender
 s = socket(AF_INET,SOCK_DGRAM)
 s.bind((host,port))
 
 addr = (host,port)
-buf = 1024
+buf=1024
+seqnum = 0
+ack = "Acknowledgement"
+
+
 
 data,addr = s.recvfrom(buf)
-result = ""
+
+#here, we need to separate the data brought in to two separate objects for data and seqnum
+#then compare seqnum of received data to counter within receiver
+
+
+
 try:
-	while(data):
-		result = data.decode('utf-8')
-		result = json.loads(result)
-#		print(result.strip("\n"))
-		print((result["data"]).strip("\n"))
-		s.settimeout(2)
-		data,addr = s.recvfrom(buf)
+    while(data):
+       #write decoded data to file
+     
+       sys.stdout.write(data.decode())
+       
+       #increment counter
+       seqnum = seqnum + 1
+    
+       s.settimeout(3)
+    
+       #receive data from sender and send acknowledgement
+       data,addr = s.recvfrom(buf)
+       
+       if(s.sendto(bytes(ack, 'utf-8'), addr)):
+         sys.stderr.write( ack  + "  " + str(seqnum) + " \n ")
+         
+         sys.stderr.write("Packet received.\n")
+    
+    
 except timeout:
-	s.close()
+    s.close()
+    sys.stderr.write("File Received, exiting.\n")
+    #sys.stderr.write(decoded_dict)
+    
+  
+  
+#------------------------------main program end----------------------------------------
--- proj2/src/sender.py
+++ proj2/src/sender.py
@@ -1,43 +1,116 @@
+# SENDER.PY
+
 # ----- sender.py ------
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 from socket import *
 import sys
-import json 
-import os 
+import time
+import json
+import base64
 
-s = socket(AF_INET,SOCK_DGRAM)
 host = sys.argv[1]
 port = int(sys.argv[2])
-buf =1024
-addr = (host,port)
 
-command = "ping -qc3 " + host + " 2>&1 | awk -F\'/\' \'END{print(/^rtt/? \"\"$6\"\":\"1\")}\'"
-temp = os.popen(cmd).read()
+s = socket(AF_INET, SOCK_DGRAM)
+# s.setblocking(0)
+buf = 1024
+addr = (host, port)
 
-TimeOut_RTT = float(temp)
+# possibly needs to take this in as a string for packet and not be encoded already?
+# no, simply send buffer size to packet instead of having it read in directly
 
-datagram  = {"sequence_number": 23134,"data":""}
-datagram_buffer = []
 
-while True:
-	line = sys.stdin.readline()
-	if not line:
-		packet = json.dumps(datagram)
-		if(s.sendto(bytes(datagram,'utf-8'),addr)):
-			print("sending...")
-			#print(datagram)
-		break
-	if((sys.getsizeof(datagram["data"])-1 + sys.getsizeof(line)) < buf):
-#		data += line
-		datagram["data"] += str(line)
-	else:
-		datagram = json.dumps(datagram)
-		if(s.sendto(bytes(datagram,'utf-8'),addr)):
-			print("sending...")
-			#print(datagram)
-#		data = line
-		datagram = json.loads(datagram)
-		datagram["data"] = str(line)
+# write data in from command line
+data = sys.stdin.read().encode()
 
+"""
+start_time = time.time()
+total_kb = 0
+"""
+
+
+# -----------------------------make packets-----------------------------------
+def create_packet(data, buf):
+    # make the data into a list of bytes to iterate through
+    seqnum = 0
+
+    new_seqnum = str(seqnum)
+
+    b_arr = bytearray(data)
+    s_arr = bytearray(seqnum)
+
+    # send bytearray with sequence number
+    # b_arr.append(seqnum)
+
+    # add seqnum array and data bytearray
+    b_arr += s_arr
+
+    # pass data into packet object
+    packet = [b_arr[i:i + buf] for i in range(0, len(b_arr), buf)]
+    # https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks/312464#312464
+    # packets = packet + header
+
+    return packet
+
+
+start_time = time.time()
+total_kb = 0
+seqnum = 0
+packets = create_packet(data, buf)
+
+arr_index = 0
+ack = " "
+s.settimeout(0.5)
+
+while (arr_index < len(packets)):
+    """ idea: have packets and sequence numbers in the same dictionary """
+
+    # packets are written into dictionary with sequence number attached to array index
+    # pack = {"packets" : packets[arr_index], "seqnum" : seqnum}
+
+    if (s.sendto(packets[arr_index], addr)):
+        arr_index = arr_index + 1
+        seqnum = arr_index
+
+        # keep track of time and total kb
+        current_time = time.time()
+        elapsed_time = current_time - start_time
+        total_kb = total_kb + 1
+
+        # print("Sequence Number: " + str(pack.get('seqnum')))
+        print("Sequence Number: " + str(seqnum))
+
+        # --------------------------------------------------------------for receiving acknowledgements-----------------------------------------------------------
+
+        # set false flag
+        ack_flag = False
+
+        # waiting to receive ack from receiver
+
+        while not ack_flag:
+            try:
+                ack, addr = s.recvfrom(1024)
+
+                ack_flag = True
+
+                # print("Ack Received")
+            except timeout:
+                s.sendto(packets[arr_index], addr)
+                # s.sendto(pack.get('packets'), addr)
+                s.settimeout(2)
+        print(ack)
+
+        # ----------------------------------------------------------for receiving acks------------------------------------------------------------------
+
+# close socket once all data sent and all acks received
 s.close()
+
+sys.stderr.write("All acks received.\n")
+
+elapsed_time = float("%0.3f" % (elapsed_time))
+bytes = total_kb * 1024
+kb_sec = total_kb / elapsed_time
+kb_sec = float("%0.3f" % (kb_sec))
+print("Sent " + str(bytes) + " bytes in " + str(elapsed_time) + " seconds: " + str(kb_sec) + " kB/s")
+print("Packets sent: " + str(arr_index))
